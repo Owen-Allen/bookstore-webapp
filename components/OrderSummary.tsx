fb9c20e426ff407077fcf4f8b31499f99816e1ca
Owen-Allen <73844263+Owen-Allen@users.noreply.github.com>
2023-07-18 15:10:13 -0400

    created forms for checkout. added state for ShippingForm.

---
+++ components/OrderSummary.tsx
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from 'react'
+import { Book } from '@/pages/browse'
+
+type CartItem = {
+    id: number,
+    quantity: number
+  }
+
+interface Props {
+    items: CartItem[],
+    bookData: Book[],
+    country: string,
+    subtotal: number,
+    total: number,
+    setSubtotal: Function,
+    setTotal: Function,
+}
+
+const TAXRATE: number = 0.15
+const SHIPPINGLOOKUP = new Map([['Canada', 5], ['United States', 10], ['Mexico', 12]])
+
+export default function OrderSummary(props: Props) {
+    const { items, bookData, country, subtotal, total, setSubtotal, setTotal } = props
+    const [ shipping, setShipping ] = useState(0)
+    const [ tax, setTax ] = useState(0)
+
+    console.log(bookData)
+    console.log(items)
+    const computeTotals = () => {
+        let s = bookData.reduce( (acc,book) => {
+            let newAmount = 0
+            let index = items.findIndex(item => String(item.id) === book.isbn)
+            console.log('index')
+            console.log(index)
+            if(index !== -1){
+                //Its possible that the user removed all this item 
+                let quantity = items[index].quantity
+                newAmount = (Number(quantity) * (book.price))
+            }
+            return acc + newAmount
+        }, 0)
+        setSubtotal(s)
+        let shippingCost = SHIPPINGLOOKUP.get(country) || -1
+        setShipping(shippingCost)
+        let t = parseFloat((s * TAXRATE).toFixed(2))
+        setTax(t)
+        setTotal(s + shippingCost + t)
+    }
+
+    useEffect( () => {
+        computeTotals()
+    }, [items, bookData, country])
+    return (
+        <div className="flex flex-col bg-gray-50 rounded w-64 mt-5 p-5 mx-5 border border-lime-900">
+            <h1 className="text-lime-900 text-2xl"> Order Summary </h1>
+            <div className="flex flex-row justify-between mt-4 mb-1">
+                <a className="text-lime-900 "> Subtotal </a>
+                <a className="text-lime-900 "> ${subtotal.toFixed(2)} </a>
+            </div>
+            <div className="flex flex-row justify-between mb-1">
+                <a className="text-lime-900"> Shipping </a>
+                <a className="text-lime-900">  { shipping > 0 ? '$' + shipping.toFixed(2) : 'TBD'} </a>
+            </div>
+            <div className="flex flex-row justify-between">
+                <a className="text-lime-900 mb-4"> Tax </a>
+                <a className="text-lime-900">  ${tax.toFixed(2)} </a>
+            </div>
+            <div className="flex flex-row justify-between">
+                <a className="text-lime-900 text-lg font-bold"> Total </a>
+                <a className="text-lime-900 text-lg font-bold"> ${total.toFixed(2)} </a>
+            </div>
+        </div>
+    )
+}
---
+++ components/ReviewTable.tsx
@@ -0,0 +1,62 @@
+import { Book } from '@/pages/browse'
+import React from 'react'
+
+
+interface Props {
+    bookData: Book[],
+    getItemQuantity: Function,
+    increaseCartQuantity: Function,
+    decreaseCartQuantity: Function,
+    }
+
+export default function ReviewTable(props: Props) {
+    const { bookData, getItemQuantity, increaseCartQuantity, decreaseCartQuantity} = props
+
+    return (
+
+        <>
+        <div className="p-5 grow">
+        <div className="overflow-auto rounded-lg shadow md:block">
+         <table className="w-full">
+            <thead className="bg-gray-50 border-b-2 border-red-200 text-lime-900">
+                <tr>
+                    <th className="p-3 text-sm font-semibold tracking-wide text-left">Title</th>
+                    <th className="p-3 text-sm font-semibold tracking-wide text-left">Author</th>
+                    <th className="p-3 text-sm font-semibold tracking-wide text-left">Price</th>
+                    <th className="p-3 text-sm font-semibold tracking-wide text-left">Quantity</th>
+                    <th className="p-3 text-sm font-semibold tracking-wide text-left">Total</th>
+                </tr>
+            </thead>
+
+            <tbody className="divide-y divide-red-100">
+                {bookData.map((book) =>
+                    <tr key={`${book.isbn}`} className="bg-gray-50">
+                        <td className="p-3 text-sm text-lime-900 whitespace-nowrap">
+                            <a href={"/book/" + book.isbn} className="font-semibold text-lime-900 hover:underline"> {book.title}</a>
+                        </td>
+                        <td className="p-3 text-sm text-lime-900 whitespace-nowrap">
+                            {book.author}
+                        </td>
+                        <td className="p-3 text-sm text-lime-900 whitespace-nowrap">
+                            {'$' + Number(book.price).toFixed(2)}
+                        </td>
+                        <td className="text-sm text-lime-900 whitespace-nowrap">
+                            <div className="flex justify-start mt-1">
+                                <button onClick={() => decreaseCartQuantity(+book.isbn)} className="font-bold rounded-full mt-1 w-6 h-6 bg-orange-300 text-black"> – </button>
+                                <div className="text-black border-black rounded-sm w-8 font-bold p-2 flex justify-center">
+                                    <a>{getItemQuantity(+book.isbn)}</a>
+                                </div>
+                                <button onClick={() => increaseCartQuantity(+book.isbn)} className="font-bold rounded-full mt-1 w-6 h-6 bg-orange-300 text-black"> ＋ </button>
+                            </div>
+                        </td>
+                        <td className="w-32 p-3 text-sm text-gray-700 whitespace-nowrap">{'$' + (getItemQuantity(+book.isbn) * book.price).toFixed(2)}</td>
+                    </tr>
+
+                )}
+            </tbody>
+        </table>
+        </div>
+        </div>
+        </>
+    )
+}
