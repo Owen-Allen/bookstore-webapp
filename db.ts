e73541a52999d5dece5f042a17aed14c1237743d
Owen-Allen <73844263+Owen-Allen@users.noreply.github.com>
2023-07-19 02:00:46 -0400

    updated db config

--- db.ts
+++ db.ts
@@ -1,14 +1,31 @@
-import { PrismaClient } from '@prisma/client'
+// import { PrismaClient } from '@prisma/client'
 
 // https://www.prisma.io/docs/guides/other/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClient | undefined
+// }
+
+// export const prisma =
+//   globalForPrisma.prisma ??
+//   new PrismaClient()
+
+// // if (process.env.NODE_ENV !== 'production') 
+// globalForPrisma.prisma = prisma
+import { PrismaClient } from '@prisma/client';
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  let globalWithPrisma = global as typeof globalThis & {
+    prisma: PrismaClient;
+  };
+  if (!globalWithPrisma.prisma) {
+    globalWithPrisma.prisma = new PrismaClient();
+  }
+  prisma = globalWithPrisma.prisma;
 }
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient()
-
-// if (process.env.NODE_ENV !== 'production') 
-globalForPrisma.prisma = prisma
+export default prisma;
