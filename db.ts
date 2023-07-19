aeab40c14ee1bd3d1d6c410772f790a29c90e6b2
Owen-Allen <73844263+Owen-Allen@users.noreply.github.com>
2023-07-19 01:37:09 -0400

    testing change

--- db.ts
+++ db.ts
@@ -10,4 +10,5 @@ export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient()
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+// if (process.env.NODE_ENV !== 'production') 
+globalForPrisma.prisma = prisma
